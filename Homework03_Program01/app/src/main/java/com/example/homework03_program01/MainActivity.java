4143529cb2807f4ef740bcb2630a069fce78e91b
athebolt <themanwiththeplan01@icloud.com>
2023-11-13 15:08:20 -0500

    whitespace change
    whitespace change

--- Homework03_Program01/app/src/main/java/com/example/homework03_program01/MainActivity.java
+++ Homework03_Program01/app/src/main/java/com/example/homework03_program01/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity
             User userPassed = (User) cameFrom.getSerializableExtra("User");
 
             listOfUsers.add(userPassed);
-
         }
 
         addButtonEventListener();
